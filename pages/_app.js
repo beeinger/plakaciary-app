a47fdb5ef3c409099a00dd0e8c0152a772a0b258
beeinger <sulecki.maciek@gmail.com>
2020-11-17 17:39:50 +0000

    quick fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "components/Layout";
 import GlobalContext, { useData, useSearch } from "context";
 import { ToastContainer } from "react-toastify";
