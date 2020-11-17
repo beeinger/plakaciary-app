df167def5a8ed7f8f4c79b8bb3ad2d5ef5e80adf
beeinger <sulecki.maciek@gmail.com>
2020-11-17 17:36:29 +0000

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "components/Layout";
 import GlobalContext, { useData, useSearch } from "context";
 import { ToastContainer } from "react-toastify";
+import ReactGA from "react-ga";
+import { useRouter } from "next/router";
 
 import "react-toastify/dist/ReactToastify.css";
 import "pure-react-carousel/dist/react-carousel.es.css";
@@ -9,7 +11,18 @@ import "./index.css";
 
 function MyApp({ Component, pageProps }) {
   const data = useData(),
-    search = useSearch();
+    search = useSearch(),
+    router = useRouter();
+
+  useEffect(() => {
+    ReactGA.initialize("UA-183254663-2");
+  }, []);
+
+  useEffect(() => {
+    ReactGA.set({ page: router.pathname });
+    ReactGA.pageview(router.pathname);
+  }, [router.pathname]);
+
   return (
     <GlobalContext.Provider
       value={{
