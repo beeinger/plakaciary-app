bfebb35757562106ccd0c880ba9ed2d66d7a8deb
beeinger <sulecki.maciek@gmail.com>
2021-02-27 19:09:52 +0000

    quick fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 const withOptimizedImages = require("next-optimized-images");
-const withProgressBar = require("next-progressbar");
 const path = require("path");
 
-module.exports = withProgressBar(
-  withOptimizedImages({
-    webpack(config) {
-      config.resolve.alias.images = path.join(__dirname, "images");
-      return config;
-    },
-    serverRuntimeConfig: {
-      PROJECT_ROOT: __dirname,
-    },
-  })
-);
+module.exports = withOptimizedImages({
+  webpack(config) {
+    config.resolve.alias.images = path.join(__dirname, "images");
+    return config;
+  },
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+  },
+});
